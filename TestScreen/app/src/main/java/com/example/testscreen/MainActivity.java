b8661fc699dac123637c95ef054660cae5a4b3ef
Daniel Rong <ddr42@cornell.edu>
2019-07-28 20:08:33 -0400

    android TestScreen app part 7 (interactivity)

--- TestScreen/app/src/main/java/com/example/testscreen/MainActivity.java
+++ TestScreen/app/src/main/java/com/example/testscreen/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
     String message = editText.getText().toString();
     intent.putExtra("com.example.testscreen.MESSAGE", message);
     startActivity(intent);
+  }
+
+  public void toastMe(View view) {
+    Toast toast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT);
+    toast.show();
+  }
+
+  public void countMe(View view) {
+    TextView countText = (TextView)findViewById(R.id.count_text);
+    Integer count = Integer.parseInt(countText.getText().toString());
+    count++;
+    countText.setText(count.toString());
   }
 }
