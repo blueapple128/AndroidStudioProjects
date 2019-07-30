b2a7d45cab2767026ee574d6538fd0a1dae4695d
Daniel Rong <ddr42@cornell.edu>
2019-07-30 15:13:07 -0400

    android TestScreen app part 9 (gradle config experimentation and logging experimentation)

--- TestScreen/app/src/main/java/com/example/testscreen/MainActivity.java
+++ TestScreen/app/src/main/java/com/example/testscreen/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.testscreen;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+  private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Log.d(LOG_TAG, "foo");
+    try {
+      throw new Exception();
+    } catch (Exception e) {
+      Log.e(LOG_TAG, Log.getStackTraceString(e), e);
+    }
   }
 
   public void sendMessage(View view) {
